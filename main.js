0c7768c4959f2b976e98b92d5545d126e6e98013
Amash-rehmani123 <160319142+Amash-rehmani123@users.noreply.github.com>
2024-04-05 13:47:26 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,31 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    {
+        type: "number",
+        name: "numberone",
+        message: "kindly enter your first number",
+    },
+    { type: "number", name: "numbertwo", message: "kindly enter your numbertwo" },
+    {
+        type: "list",
+        name: "operator",
+        message: "select on of the operator to perfom operation",
+        choices: ["+", "-", "*", "/"],
+    },
+]);
+if (answer.operator === "+") {
+    console.log(answer.numberone + answer.numbertwo);
+}
+else if (answer.operator === "-") {
+    console.log(answer.numberone - answer.numbertwo);
+}
+else if (answer.operator === "*") {
+    console.log(answer.numberone * answer.numbertwo);
+}
+else if (answer.operator === "/") {
+    console.log(answer.numberone / answer.numbertwo);
+}
+else {
+    ("please select a valid operator");
+}
